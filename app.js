c0f2b0dc192576fc23d5d0ca8ea546411452da35
nadeemhh <95242318+nadeemhh@users.noreply.github.com>
2024-05-11 15:20:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 app.use(express.json())
+let cors = require('cors')
+app.use(cors())
+const { createTransport } = require('nodemailer');
 const publicDirectoryPath = path.join(__dirname, './client')
 app.use(express.static(publicDirectoryPath))
 const port = process.env.PORT || 3400
@@ -9,5 +12,40 @@ const port = process.env.PORT || 3400
 
 
 
+app.post('/sendmail', (req, res) => {
+
+  
+  const transporter = createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'foranyuse2221@gmail.com',
+      pass: 'xyvzcmzzrgeiffvg'
+    }
+});
+
+const mailOptions = {
+    from: 'foranyuse2221@gmail.com',
+    to: 'foranyuse2221@gmail.com',
+    subject: `mail from websolve`,
+    html: `<p>
+    Name: ${req.body.name}<br>
+    Email: ${req.body.email}<br>
+    Subject: ${req.body.subject}<br>
+    Message: ${req.body.message}
+</p>`
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
+  
+  res.send(JSON.stringify({ message: 'Mail Sent Successfully' }));
+});
+
 app.listen(port)
+
 
